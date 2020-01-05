ec94468421eb1fd1cd2ff67381d266b572e868d8
root <root@panel.stebler>
2020-01-05 19:25:13 +0100

    subo todo

---
+++ convertir.php
@@ -0,0 +1,6 @@
+<?php 
+
+echo shell_exec("prueba.sh");
+
+echo "no se han enviado datos";
+?>
---
+++ out.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mosquitto_pub -h gzih-test.gozmart.ch -t gozmart/sonoff/2CF432787770110511/on/set -m true
